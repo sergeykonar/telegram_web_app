d2191d305fc7bf593e06444bc793629f3b593697
sergeykonar <sergey.konar@qubixstudio.com>
2024-06-07 21:55:51 +0200

    Contacts

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,20 @@ import './App.css';
 const tg = window.Telegram.WebApp;
 
 
+
 function App() {
   useEffect(() => {
     tg.ready()
   }, [])
 
   const onClose = () => {
-    tg.sendData("/test")
+    tg.requestContact((success: boolean, response: RequestContactResponse) => {
+      if (success) {
+          console.log('Contact received:', response);
+      } else {
+          console.error('Failed to receive contact');
+      }
+  });
   }
   return (
     <div className="App">
